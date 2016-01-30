065cb9046aa5127dbdba1b9ad0de74f779e0b1fd
Nik Nyby <nikolas@gnu.org>
2016-01-29 23:23:53 -0500

    initial commit

---
+++ quickstart.py
@@ -0,0 +1,74 @@
+from __future__ import unicode_literals
+
+import httplib2
+import os
+
+from apiclient import discovery
+import oauth2client
+from oauth2client import client, tools
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Gmail API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'gmail-python-quickstart.json')
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else:
+            # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+            print('Storing credentials to ' + credential_path)
+    return credentials
+
+
+def main():
+    """Shows basic usage of the Gmail API.
+
+    Creates a Gmail API service object and outputs a list of label names
+    of the user's Gmail account.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+
+    result = service.users().messages().list(
+        userId='me', maxResults=1).execute()
+    msgid = result['messages'][0]['id']
+    result = service.users().messages().get(
+        userId='me', id=msgid).execute()
+    for header in result['payload']['headers']:
+        if header['name'] == 'Subject':
+            subject = header['value']
+            break
+
+    print(subject)
+
+
+if __name__ == '__main__':
+    main()
